160422f8e88d0b7c3f414aa7c42af467621d0c5a
Nicole Muzzin <nick.muzzin@gmail.com>
2022-02-10 21:21:09 -0500

    Added ::ffff:127.0.0.1/72
    Golly gee I sure hope that's the right mask

--- src/IpRanges.ts
+++ src/IpRanges.ts
@@ -1,7 +1,8 @@
 export class IpRanges {
     static readonly local = [
         "127.0.0.0/8",
-        "::1/128"
+        "::1/128",
+        "::ffff:127.0.0.1/72"
     ]
     static readonly all = [
         "0.0.0.0/0",
