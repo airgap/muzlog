9074a8e96d2ec6428c8eb7d58f766fcdc906a1bd
Nicole Muzzin <nick.muzzin@gmail.com>
2022-02-13 07:00:53 -0500

    en -> Filter.evaluateFilterNode

--- src/Operators.ts
+++ src/Operators.ts
@@ -1,23 +1,22 @@
 import { Filter } from './Filter.js';
 export type Operator = (packet: object, parameters: unknown[]) => any;
 const
-    en = Filter.evaluateFilterNode,
     And: Operator = (packet, parameters) => Filter.evaluateReduce(parameters, packet, (a: any, b: any)=>a&&b),
     Nand: Operator = (n, p) => !And(n, p),
     Or: Operator = (packet, parameters) => Filter.evaluateReduce(parameters, packet, (a: any, b: any)=>a||b),
     Nor: Operator = (n, p) => !Or(n, p),
     Equals: Operator = (packet, parameters) => {
-        const paramZero = en(packet, parameters.splice(0,1)[0]);
+        const paramZero = Filter.evaluateFilterNode(packet, parameters.splice(0,1)[0]);
         for (let p of parameters)
-            if(paramZero != en(packet, p))
+            if(paramZero != Filter.evaluateFilterNode(packet, p))
                 return !1;
         return !0;
     },
     Nequals: Operator = (n, p) => !Equals(n, p),
     Under: Operator = (packet, parameters) => {
         for (let p = 1; p < parameters.length; p++)
-            if(en(packet, parameters[p-1])
-                >= en(packet, parameters[p]))
+            if(Filter.evaluateFilterNode(packet, parameters[p-1])
+                >= Filter.evaluateFilterNode(packet, parameters[p]))
                 return !1;
         return !0;
     },
@@ -28,9 +27,9 @@ const
     Div: Operator = (c, m) => Filter.evaluateReduce(m, c, (a: any, b: any)=>a/b),
     Exp: Operator = (c, m) => Filter.evaluateReduce(m, c, (a: any, b: any)=>a**b),
     Includes: Operator = (packet, parameters) => {
-        const paramZero = en(packet, parameters.splice(0,1)[0]);
+        const paramZero = Filter.evaluateFilterNode(packet, parameters.splice(0,1)[0]);
         for (let p of parameters)
-            if(paramZero.includes(en(packet, p)))
+            if(paramZero.includes(Filter.evaluateFilterNode(packet, p)))
                 return !1;
         return !0;
     };
