cd8c7aa7881e9c85fdbc0456eeea2210b1983b6e
Nicole Muzzin <nick.muzzin@gmail.com>
2022-02-13 06:32:41 -0500

    why are we here
    just to suffer

--- src/Operators.ts
+++ src/Operators.ts
@@ -1,11 +1,10 @@
 import { Filter } from './Filter.js';
 export type Operator = (packet: object, parameters: unknown[]) => any;
 const
-    er = Filter.evaluateReduce,
     en = Filter.evaluateFilterNode,
-    And: Operator = (packet, parameters) => er(parameters, packet, (a: any, b: any)=>a&&b),
+    And: Operator = (packet, parameters) => Filter.evaluateReduce(parameters, packet, (a: any, b: any)=>a&&b),
     Nand: Operator = (n, p) => !And(n, p),
-    Or: Operator = (packet, parameters) => er(parameters, packet, (a: any, b: any)=>a||b),
+    Or: Operator = (packet, parameters) => Filter.evaluateReduce(parameters, packet, (a: any, b: any)=>a||b),
     Nor: Operator = (n, p) => !Or(n, p),
     Equals: Operator = (packet, parameters) => {
         const paramZero = en(packet, parameters.splice(0,1)[0]);
@@ -23,11 +22,11 @@ const
         return !0;
     },
     Nunder: Operator = (n, p) => !Under(n, p),
-    Add: Operator = (c, m) => er(m, c, (a: any, b: any)=>a+b),
-    Sub: Operator = (c, m) => er(m, c, (a: any, b: any)=>a-b),
-    Mul: Operator = (c, m) => er(m, c, (a: any, b: any)=>a*b),
-    Div: Operator = (c, m) => er(m, c, (a: any, b: any)=>a/b),
-    Exp: Operator = (c, m) => er(m, c, (a: any, b: any)=>a**b),
+    Add: Operator = (c, m) => Filter.evaluateReduce(m, c, (a: any, b: any)=>a+b),
+    Sub: Operator = (c, m) => Filter.evaluateReduce(m, c, (a: any, b: any)=>a-b),
+    Mul: Operator = (c, m) => Filter.evaluateReduce(m, c, (a: any, b: any)=>a*b),
+    Div: Operator = (c, m) => Filter.evaluateReduce(m, c, (a: any, b: any)=>a/b),
+    Exp: Operator = (c, m) => Filter.evaluateReduce(m, c, (a: any, b: any)=>a**b),
     Includes: Operator = (packet, parameters) => {
         const paramZero = en(packet, parameters.splice(0,1)[0]);
         for (let p of parameters)
