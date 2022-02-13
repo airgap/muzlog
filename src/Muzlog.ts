5c09e9c11ff421bad2a7d0afdd5b2be81ca8f353
Nicole Muzzin <nick.muzzin@gmail.com>
2022-02-13 01:17:33 -0500

    Oh gods what didn't i change

--- src/Muzlog.ts
+++ src/Muzlog.ts
@@ -1,11 +1,13 @@
 import dash from 'rethinkdbdash';
 import {readFile} from "fs/promises";
 import {join} from 'path';
-import {Server} from 'https';
+import {Server as HttpsServer} from 'https';
 import * as routes from './routes/routes.js';
-import {RequestListener} from 'http';
+import {RequestListener, Server as HttpServer} from 'http';
 import {Route} from "./Route.js";
 import fetch from 'node-fetch';
+import WebSocket, {WebSocketServer} from 'ws';
+import {parse} from 'url';
 
 const r: any = dash({
     servers: [{
@@ -22,7 +24,7 @@ export class Muzlog {
         return [path, route];
     }));
     _certs?: { [key: string]: string };
-    api?: Server;
+    api?: HttpsServer | HttpServer;
 
     beatInterval?: NodeJS.Timer;
     heartrate = 10000;
@@ -30,26 +32,28 @@ export class Muzlog {
     constructor() {
     }
 
-    startApi = async () => {
-        this.api = new Server(await this.certs(
-            '/etc/letsencrypt/live/log.muzz.in-0001',
-            {
-                cert: 'fullchain',
-                key: 'privkey',
-                ca: 'chain'
-            }
-        ), this.listener).listen(443);
-        this.startHeatbeat();
+    startHttpApi = async () => {
+        const names = ['cert', 'key', 'ca'];
+        const paths = names.map(name => process.env[name]);
+        if (paths.some(p => p)) {
+            if (!paths.every(p => p))
+                throw new Error('Some but not all cert paths set');
+            const [cert, key, ca] = await Promise.all(paths.map(path => readFile(path!, 'utf8')));
+            this.api = new HttpsServer({cert, key, ca}, this.httpListener).listen(443);
+        } else {
+            this.api = new HttpServer(this.httpListener).listen(80);
+        }
+        this.startHeartbeat();
     }
 
-    startHeatbeat = () => {
+    startHeartbeat = () => {
         if (this.beatInterval)
             clearInterval(this.beatInterval);
         console.log('Starting heartbeat');
         this.beatInterval = setInterval(this.heartbeat, this.heartrate);
     }
     heartbeat = async () => {
-        console.log('Trying to beat own heart');
+        // console.log('Trying to beat own heart');
         const now = +new Date();
         await fetch('https://log.muzz.in/beatOwnHeart', {
             method: 'POST',
@@ -65,7 +69,7 @@ export class Muzlog {
         })
     }
 
-    listener: RequestListener = (req, res) => {
+    httpListener: RequestListener = (req, res) => {
         let path = req?.url?.substring(1);
         if (path && path.length)
             path = path[0].toUpperCase() + path.substring(1);
@@ -74,6 +78,26 @@ export class Muzlog {
             res.end('Giggity');
             return;
         }
+        req.on('upgrade', (request, socket, head) => {
+
+            const {pathname} = parse(request.url);
+
+            if (pathname?.substring(0, 4) === '/ws/') {
+                if(!this.wsServer) {
+                    console.error('Request made to offline WebSocket Server')
+                    return;
+                }
+                this.wsServer.handleUpgrade(request, socket, head, (ws: WebSocket) => {
+                    if(!this.wsServer) {
+                        console.error('HTTP->WebSocket upgrade failed');
+                        return;
+                    }
+                    this.wsServer.emit('connection', ws, request);
+                });
+            } else {
+                socket.destroy();
+            }
+        });
         if (path in routes) {
             let data: string[] = [];
             req.on('data', (chunk) =>
@@ -109,10 +133,47 @@ export class Muzlog {
         }
     }
 
-    init = () =>
-        Promise.all([
-            this.startApi()
-        ]);
+    sockets = new WeakSet<WebSocket>();
+    wsServer?: WebSocketServer;
+
+    startWsApi = () => {
+        this.wsServer = new WebSocketServer({
+            port: 444,
+            perMessageDeflate: {
+                zlibDeflateOptions: {
+                    // See zlib defaults.
+                    chunkSize: 1024,
+                    memLevel: 7,
+                    level: 3
+                },
+                zlibInflateOptions: {
+                    chunkSize: 10 * 1024
+                },
+                // Other options settable:
+                clientNoContextTakeover: true, // Defaults to negotiated value.
+                serverNoContextTakeover: true, // Defaults to negotiated value.
+                serverMaxWindowBits: 10, // Defaults to negotiated value.
+                // Below options specified as default values.
+                concurrencyLimit: 10, // Limits zlib concurrency for perf.
+                threshold: 1024 // Size (in bytes) below which messages
+                // should not be compressed if context takeover is disabled.
+            }
+        })
+
+        this.wsServer.on('connection', (ws: WebSocket) => {
+            this.sockets.add(ws);
+            ws.on('message', (data: string) => {
+                console.info('received: %s', data);
+            });
+        });
+        console.info('WebSocket server started');
+    }
+
+
+    init = async () => {
+        this.startWsApi()
+        await this.startHttpApi();
+    }
 
     certs = async (dir: string, certs: { [key: string]: string }) =>
         this._certs = Object.fromEntries(
