84e05e676095791249022e3eac81c629316c3ee6
Nicole Muzzin <nick.muzzin@gmail.com>
2022-02-13 21:18:05 -0500

    Delete secret from event before logging it because duh

--- src/routes/TeamcityWebhook.ts
+++ src/routes/TeamcityWebhook.ts
@@ -7,7 +7,7 @@ export class TeamcityWebhook extends Route {
         console.log("teamcityWebhook", params);
         if(params.secret !== process.env.TEAMCITY_WEBHOOK_SECRET)
             return {status: 403, body: "YAH YEET"};
-
+        delete params.secret;
         await insertEvent(
             'ci/teamcity/webhook',
             params,
